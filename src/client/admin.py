8d29a8d643e9e88ceeb45a6c7f8abd738446196b
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-10-07 21:11:12 +0545

    adding client and artist model

--- src/client/admin.py
+++ src/client/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import client, artist
 
 # Register your models here.
+admin.site.register(client)
+admin.site.register(artist)
--- src/client/models.py
+++ src/client/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False)
+    profile_photo = models.ImageField(
+        default="profile.png", upload_to='profile/')
+    bio = models.CharField(max_length=200, default="no bio...")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, blank=True)
+    is_client = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username}-{self.date_created.strftime('%d-%m-%Y')}"
+
+    def get_user_email(self):
+        return self.user.email
+
+
+class artist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(
+        default="profile.png", upload_to='profile/')
+    bio = models.CharField(max_length=200, default="no bio...")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, blank=True)
+    is_artist = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username}-{self.date_created.strftime('%d-%m-%Y')}"
+
+    def get_user_email(self):
+        return self.user.email
