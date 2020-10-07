ad983049326ecb684de67869ffae2af5ca110ddd
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-10-07 18:24:24 +0545

    documenting the authentication app

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -4,8 +4,18 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
+    '''
+    inherits the UserCreationForm
+
+    this class is used for extending the UserCreationForm
+    helps to specify which field i like when we render form
+    '''
     email = forms.EmailField()
 
+    # meta class is used to descrive the extra information about class
     class Meta:
+        # uses User Class as model
         model = User
+        # and this fields is used to specify which input fields going to
+        # render in signup page
         fields = ["username", "email", "password1", "password2"]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -2,14 +2,26 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
 # Create your views here.
+
+
 def register(response):
-    if response.method == "POST":
+    '''
+    the view function is called when user go the signup page
+
+    since, signup is post request we first check 
+    the response method is post to do sign up
+
+    '''
+    if response.method == "POST":  # if the request method is post then
+        # converting the response.POST to form using RegisterForm class
         form = RegisterForm(response.POST)
-        if form.is_valid():
+        if form.is_valid():  # check is the form valid then
+            # form is saved to the table specifed in RegisterForm Class
             form.save()
-
+            # finally reurning to /home url
             return redirect("/home")
-    else:
-	    form = RegisterForm()
-
-    return render(response, "signup.html", {"form":form})
+    else:  # if the requset method is not post eg. get then
+        # form is created
+        form = RegisterForm()
+    # finally redering the form in the signup page
+    return render(response, "signup.html", {"form": form})
