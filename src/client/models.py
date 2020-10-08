ff0d7a9a3508ca9c5cf74cb4c880bc87b84e86ae
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-10-08 16:43:48 +0545

    link artGallery to artist for adding art

--- src/client/models.py
+++ src/client/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from artGallery.models import art
 
 # Create your models here.
 
@@ -12,7 +13,7 @@ class client(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True, blank=True)
-    is_client = models.BooleanField(default=True)
+    is_client = models.BooleanField(choices=[(True, "yes"), ], default=True)
 
     def __str__(self):
         return f"{self.user.username}-{self.date_created.strftime('%d-%m-%Y')}"
@@ -22,17 +23,19 @@ class client(models.Model):
 
 
 class artist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False)
     profile_photo = models.ImageField(
         default="profile.png", upload_to='profile/')
     bio = models.CharField(max_length=200, default="no bio...")
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True, blank=True)
-    is_artist = models.BooleanField(default=True)
+    is_artist = models.BooleanField(choices=[(True, "Yes"), ], default=True)
+    arts = models.ForeignKey(
+        art, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username}-{self.date_created.strftime('%d-%m-%Y')}"
 
     def get_user_email(self):
-        return self.user.email
+        return f"{self.user.email}"
