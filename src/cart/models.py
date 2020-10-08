0af89e1dcbedcf214e17d8ca08bb2f625b93a092
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-10-08 17:48:35 +0545

    modeling cart

--- src/cart/models.py
+++ src/cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from artGallery.models import art
 
 # Create your models here.
+
+
+class cart(models.Model):
+    arts = models.ManyToManyField(art, related_name="cart_art")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.date_created.strftime('%d-%m-%y')}"
