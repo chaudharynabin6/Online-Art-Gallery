2105a1bdeb37e38e31bc43d6f63fefe27cf93b40
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-11-23 22:28:27 +0545

    setting up postgresql as database

--- src/online_art_gallery/settings.py
+++ src/online_art_gallery/settings.py
@@ -92,10 +92,27 @@ WSGI_APPLICATION = 'online_art_gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # requied for which database engine
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # database name
+        'NAME': 'online-art-gallery',
+        # user that can login to postgres
+        'USER': 'postgres',
+        # password for user
+        'PASSWORD': 'admin123',
+        # server address
+        'HOST': 'localhost',
+        # post of server
+        'PORT': '5432',
     }
 }
 
