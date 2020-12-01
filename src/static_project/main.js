9f7c70260318db627be8e4168123f6cd644116fa
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-12-01 20:34:56 +0545

    navbar: adding newnavbar and fixing some issues

--- src/static_project/main.js
+++ src/static_project/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
   console.log("base is working");
 });
+//-------------------------------------------
+// nav bar js
+(function () {
+  const mainHeader = document.querySelector(".mainHeader");
+  window.addEventListener("scroll", function () {
+    const isScrlled = window.scrollY;
+    if (isScrlled > 10) {
+      mainHeader.classList.add("scrolled");
+    } else {
+      mainHeader.classList.remove("scrolled");
+    }
+  });
+})();
+
+// ------------------------------------------------
