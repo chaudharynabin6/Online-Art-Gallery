f63065888c04e9ba417aa3b74775a5a8b7e40cc7
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-12-03 18:21:46 +0545

    implementing exhibition cart

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import view_art_gallery_cart
+from .views import view_art_gallery_cart, exhibition_cart
 app_name = "cart"
 urlpatterns = [
     path("", view_art_gallery_cart, name="cart"),
+    path("exhibition-cart", exhibition_cart, name="exhibition-cart")
 ]
--- src/cart/views.py
+++ src/cart/views.py
@@ -1,7 +1,12 @@
-from django.http import request
+
+from decimal import Context
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from client.models import client, artist
 from django.db.models import Sum
+from exhibition.models import exhibition, art as artClass, auction
+from django.db.models import Max
+from decimal import Decimal
 # Create your views here.
 
 
@@ -37,3 +42,52 @@ def view_art_gallery_cart(request):
             return render(request, "client/not-found.html", {
                 "error": "you must be client"
             })
+
+
+def create_cart_from_exhibition():
+    """
+    get the client,art,auction,
+    """
+    max_auction_list = []
+    ex = exhibition.objects.filter(is_active=True).first()
+    if(ex):
+        approved_arts = artClass.objects.filter(
+            exhibition=ex, is_approved=True)
+        if(approved_arts):
+            for art in approved_arts.iterator():
+                auctions = auction.objects.filter(art=art)
+                if(auctions):
+                    max_auction = auctions.order_by('-bid_amount')[0]
+                    max_auction_list.append(max_auction)
+
+            return max_auction_list
+
+
+def exhibition_cart(request):
+    if not request.user.is_authenticated or request.user.is_superuser:
+        return render(request, "exhibition/not-found.html", {
+            "error": "you must login as client"
+        })
+    current_client = client.objects.filter(user=request.user).first()
+    if(current_client):
+        my_auctions = []
+        total_amount = Decimal(0)
+        max_auction_list = create_cart_from_exhibition()
+        if(max_auction_list):
+            for item in max_auction_list:
+                my_auc = item.client == current_client
+                if(my_auc):
+                    my_auctions.append(item)
+                    total_amount += item.bid_amount
+
+        context = {
+            "my_auctions": my_auctions,
+            "total_amount": total_amount,
+            "has_art": bool(my_auctions),
+        }
+        print(context)
+        return render(request, "cart/exhibition-cart.html", context)
+    else:
+        return render(request, "client/not-found.html", {
+            "error": "you must be client"
+        })
