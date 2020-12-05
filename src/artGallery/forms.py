087b4f9c3ce88601649c410207a85707969ad5cc
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-12-05 21:05:26 +0545

    adding art description in art showcase and form

--- src/artGallery/forms.py
+++ src/artGallery/forms.py
@@ -6,5 +6,5 @@ class art_add_or_update(forms.ModelForm):
 
     class Meta:
         model = art
-        fields = ["art_name", "quality", "price", "photo1",
+        fields = ["art_name", "quality", "price", "description", "photo1",
                   "photo2", "photo3", "photo4", "photo5"]
