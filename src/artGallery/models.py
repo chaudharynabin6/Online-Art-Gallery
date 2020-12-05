c76146f78eb37996dc3cdb694e2353ab617f6f3f
chaudharynabin6 <58876071+chaudharynabin6@users.noreply.github.com>
2020-12-05 18:46:03 +0545

    changing the model of artGallery: art

--- src/artGallery/models.py
+++ src/artGallery/models.py
@@ -11,20 +11,21 @@ class art(models.Model):
     art_name = models.CharField(max_length=200, blank=False)
     quality = models.IntegerField(choices=rating_choise, default=1)
     price = models.DecimalField(decimal_places=2, max_digits=20, blank=False)
+    description = models.CharField(max_length=200, blank=False)
     art_slug = models.SlugField(
         unique=True, null=True, blank=True, editable=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     photo1 = models.ImageField(
-        upload_to="photos/", blank=False, default="profile.png")
+        upload_to="photos/", blank=False)
     photo2 = models.ImageField(
-        upload_to="photos/", blank=False, default="profile.png")
+        upload_to="photos/", blank=False)
     photo3 = models.ImageField(
-        upload_to="photos/", blank=False, default="profile.png")
+        upload_to="photos/", blank=False)
     photo4 = models.ImageField(
-        upload_to="photos/", blank=False, default="profile.png")
+        upload_to="photos/", blank=False)
     photo5 = models.ImageField(
-        upload_to="photos/", blank=False, default="profile.png")
+        upload_to="photos/", blank=False)
 
     def __str__(self):
         return f"{self.art_slug}-{self.date_created.strftime('%d-%m-%y')}"
